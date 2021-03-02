c25c8d3c374a06a4438d490bb9ca513c111f0553
unich720 <63234732+unich720@users.noreply.github.com>
2021-03-02 17:47:50 +0300

    Add files via upload

---
+++ Task_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+    class Task_2
+    {
+        interface ITransport
+        {
+            void Move();
+        }
+        class Boat : ITransport
+        {
+            public void Move()
+            {
+                Console.WriteLine("плыть");
+            }
+        }
+        class Car : ITransport
+        {
+            public void Move()
+            {
+                Console.WriteLine("ехать");
+            }
+        }
+        class Аirplane : ITransport
+        {
+            public void Move()
+            {
+                Console.WriteLine("лететь");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var obj = new Boat();
+            obj.Move();
+
+        }
+    }
+}
