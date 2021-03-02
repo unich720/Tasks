70d7c6c9505d8458d3d3e82501b045a60475fccf
unich720 <63234732+unich720@users.noreply.github.com>
2021-03-02 17:50:11 +0300

    Add files via upload

---
+++ Task_3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+    class Task_3
+    {
+        abstract class Base
+        {
+            public Base(int i)
+            {
+
+            }
+        }
+        class Foo_1 : Base
+        {
+            public Foo_1(string s) : base(s != null ? s.Length : throw new ArgumentNullException())
+            {
+
+            }
+        }
+
+        class Foo_2 : Base
+        {
+            public Foo_2(string s) : base(Foo_2.CheckNull(s))
+            {
+
+            }
+            static int CheckNull(string s)
+            {
+                if (s != null)
+                    return s.Length;
+                else
+                    throw new ArgumentNullException();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string s = null;
+            var f_1 = new Foo_1(s);
+            var f_2 = new Foo_2(s);
+
+        }
+    }
+}
