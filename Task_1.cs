27645a365c4033e3bcc174cdb52669365df8e1fc
mikerush321 <63234732+mikerush321@users.noreply.github.com>
2021-03-20 14:17:33 +0300

    up

--- Task_1.cs
+++ Task_1.cs
@@ -40,10 +40,7 @@ namespace test
         /// Свойство Path возвращающий значение в double и принимающий значение
         ///</value>
         public double Path { get; private set; }
-        ///<summary>
-        /// Переменная для перевода миль в км
-        ///</summary>
-        private double KmOnMiles { get; set; }
+
         ///<summary>
         /// Конструктор километров 
         ///</summary>
@@ -53,7 +50,7 @@ namespace test
         public KmPAth(double path)
         {
             Path = path;
-            KmOnMiles = 0.6214;
+            _kmPerMiles = 0.6214;
         }
         ///<summary>
         /// Метод добавления милий к км
@@ -63,8 +60,12 @@ namespace test
         ///</param>
         public void Add(MilesPath path)
         {
-            Path = path.Path * KmOnMiles;
+            Path = path.Path * _kmPerMiles;
         }
+        ///<summary>
+        /// Переменная для перевода миль в км
+        ///</summary>
+        private double _kmPerMiles;
     }
     ///<summary>
     /// Структура миль для перевода из км в мили
@@ -79,10 +80,6 @@ namespace test
         ///</value>
         public double Path { get; private set; }
         ///<summary>
-        /// Переменная для перевода км в миль
-        ///</summary>
-        private double MilesOnKm { get; set; }
-        ///<summary>
         /// Конструктор миль 
         ///</summary>
         ///<param name="path">
@@ -91,7 +88,7 @@ namespace test
         public MilesPath(double path)
         {
             Path = path;
-            MilesOnKm = 1.609344;
+            _milesPerKm = 1.609344;
         }
         ///<summary>
         /// Метод добавления км к милям
@@ -101,15 +98,19 @@ namespace test
         ///</param>
         public void Add(KmPAth path)
         {
-            Path = path.Path * MilesOnKm;
+            Path = path.Path * _milesPerKm;
         }
+        ///<summary>
+        /// Переменная для перевода км в миль
+        ///</summary>
+        private double _milesPerKm;
     }
     ///<summary>
     /// Метод сохраняющий единицы измерения в БД
     ///</summary>
-    public static void SavePathToDB<T>(string cargoID, IPath<T> path)
-            where T : struct, IPath
-    {
+    //public static void SavePathToDB<T>(string cargoID, IPath<T> path)
+    //        where T : struct, IPath
+    //{
 
-    }
+    //}
 }
